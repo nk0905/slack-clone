e524d148d4d3a7294465b0085351836d008e74aa
nk0905 <0dd2n.a0905@gmail.com>
2024-12-01 23:24:25 +0900

    add middleware and first page file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient} from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
-export async function updateSession(request: NextRequest) {
+export default async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   })
