b38968c52cc3f4a78f8f986ad83743fc875ed61c
nk0905 <0dd2n.a0905@gmail.com>
2024-12-02 23:50:17 +0900

    add propsType

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import { FC } from 'react'
 
-const typography = () => {
+type TypographyProps = {
+    variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
+}
+
+const typography: FC<TypographyProps> = ({ variant }) => {
   return (
     <div>typography</div>
   )
