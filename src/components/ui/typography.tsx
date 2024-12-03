73b4153fcda73a66ef197875b7b6a64e7f6a9d96
nk0905 <0dd2n.a0905@gmail.com>
2024-12-03 23:17:00 +0900

    add eslint rule

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,10 +1,12 @@
-import { FC } from 'react'
+import { FC, HTMLAttributes } from 'react'
 
 type TypographyProps = {
     variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
-}
+    text: string;
+    className?: string;
+} & HTMLAttributes<HTMLElement>
 
-const typography: FC<TypographyProps> = ({ variant }) => {
+const typography: FC<TypographyProps> = ({ variant, text, className, ...props }) => {
   return (
     <div>typography</div>
   )
