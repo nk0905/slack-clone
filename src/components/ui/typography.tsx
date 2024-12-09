c770788e5bed0d7d4359893787c68d5a14474897
nk0905 <0dd2n.a0905@gmail.com>
2024-12-09 22:39:29 +0900

    add typography className

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -6,9 +6,23 @@ type TypographyProps = {
     className?: string;
 } & HTMLAttributes<HTMLElement>
 
-const typography: FC<TypographyProps> = ({ variant, text, className, ...props }) => {
+const typography: FC<TypographyProps> = ({ variant = 'h1', text, className, ...props }) => {
+  const classNames = {
+    h1: 'scroll-m-20 text-4xl font-extra-bold tracking-tight lg:text-5xl',
+    h2: 'scroll-m-16 text-3xl font-bold tracking-tight lg:text-4xl',
+    h3: 'scroll-m-12 text-2xl font-semi-bold tracking-tight lg:text-3xl',
+    h4: 'scroll-m-10 text-xl font-medium tracking-tight lg:text-2xl', 
+    h5: 'scroll-m-8 text-lg font-normal tracking-tight lg:text-xl',
+    h6: 'scroll-m-6 text-base font-normal tracking-tight lg:text-xl',
+    p: 'scroll-m-4 text-sm font-normal tracking-tight lg:text-base',
+  };
+
+  const Tag = variant;
+  const defaultClassName = classNames[variant];
+  const combinedClassName = cn(defaultClassName, className);
+
   return (
-    <div>typography</div>
+    <Tag>typography</Tag>
   )
 }
 
