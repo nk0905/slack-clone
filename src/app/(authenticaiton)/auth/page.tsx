f8fb8e0c33dc62edebb223d6ffc57a64f982c596
nk0905 <0dd2n.a0905@gmail.com>
2025-01-02 22:40:49 +0900

    add router

--- src/app/(authenticaiton)/auth/page.tsx
+++ src/app/(authenticaiton)/auth/page.tsx
@@ -14,7 +14,8 @@ import Typography from '@/components/ui/typography';
 import { supabaseBrowserClient } from '@/supabase/supabaseClient';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Provider } from '@supabase/supabase-js';
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { BsSlack } from 'react-icons/bs';
 import { FcGoogle } from 'react-icons/fc';
@@ -24,6 +25,24 @@ import { z } from 'zod';
 
 const AuthPage = () => {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      const {
+        data: { session },
+      } = await supabaseBrowserClient.auth.getSession();
+
+      if (session) {
+        return router.push('/');
+      }
+    };
+
+    getCurrentUser();
+    setIsMounted(true);
+  }, [router]);
 
   const formSchema = z.object({
     email: z.string().email().min(2, { message: 'Email must be 2 characters' }),
@@ -56,6 +75,10 @@ const AuthPage = () => {
       },
     });
     setIsAuthenticating(false);
+  }
+
+  if (!isMounted) {
+    return null;
   }
 
   return (
