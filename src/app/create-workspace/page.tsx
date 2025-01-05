2a96cd415a4939f70a915493b2f89a3a6f2111ec
nk0905 <0dd2n.a0905@gmail.com>
2025-01-05 22:56:41 +0900

    uploadthing

--- src/app/create-workspace/page.tsx
+++ src/app/create-workspace/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
+import ImageUpload from '@/components/image-upload';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import Typography from '@/components/ui/typography';
 import { useCreateWorkspaceValues } from '@/hooks/create-workspace-values';
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreateWorkspace = () => {
   const { currStep } = useCreateWorkspaceValues();
@@ -75,5 +76,80 @@ const Step1 = () => {
 };
 
 const Step2 = () => {
-  return <div>Step2</div>;
+  const { setCurrStep, updateImageUrl, imageUrl, name } =
+    useCreateWorkspaceValues();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    setIsSubmitting(true);
+    const slug = slugify(name);
+    const invite_code = uuid();
+    const error = await createWorkspace({ imageUrl, name, slug, invite_code });
+    setIsSubmitting(false);
+    if (error?.error) {
+      console.log(error);
+      return toast.error("Couldn't create workspace. Please try again.");
+    }
+    toast.success('Workspace created successfully');
+    router.push('/');
+  };
+
+  return (
+    <>
+      <Button
+        size="sm"
+        className="text-white"
+        variant="link"
+        onClick={() => setCurrStep(1)}
+      >
+        <Typography text="Back" variant="p" />
+      </Button>
+
+      <form>
+        <Typography text="Add workspace avatar" className="my-4" />
+        <Typography
+          text="This image can be changed later in your workspace settings."
+          className="text-neutral-300"
+          variant="p"
+        />
+
+        <fieldset
+          disabled={isSubmitting}
+          className="mt-6 flex flex-col items-center space-y-9"
+        >
+          <ImageUpload />
+          <div className="space-x-5">
+            <Button
+              onClick={() => {
+                updateImageUrl('');
+                handleSubmit();
+              }}
+            >
+              <Typography text="Skip for now" variant="p" />
+            </Button>
+
+            {imageUrl ? (
+              <Button
+                type="button"
+                onClick={handleSubmit}
+                size="sm"
+                variant="destructive"
+              >
+                <Typography text="Submit" variant="p" />
+              </Button>
+            ) : (
+              <Button
+                type="button"
+                size="sm"
+                className="text-white bg-gray-500"
+              >
+                <Typography text="Select an Image" variant="p" />
+              </Button>
+            )}
+          </div>
+        </fieldset>
+      </form>
+    </>
+  );
 };
